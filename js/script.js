6ac54c3e4dab528088cd3d9b5ea81aabcc0fbd88
wonjun <wonjun_@ucsit.co.kr>
2021-10-08 10:50:03 +0900

    new file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*************************************
+*               jQuery               *
+*************************************/
+$(document).ready(function(){
+
+    
+    /********* Content Editable Div *********/
+    $('.editDiv').attr('contenteditable', 'true'); // editDiv클래스 추가하면 속성을 변경한다.
+});
